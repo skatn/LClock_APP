dea110c091d2eb58fd2778737851640d74742150
skatn <skatn6679@gmail.com>
2020-06-03 17:42:25 +0900

    okhttp3 적용

--- app/src/main/java/com/gcr/oclock/MainActivity.java
+++ app/src/main/java/com/gcr/oclock/MainActivity.java
@@ -1,14 +1,110 @@
 package com.gcr.oclock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+
+import java.io.IOException;
+
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_LOCATION = 1;
+    private static final String TAG = "qqqq";
+
+    private OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permission();
+        client = new OkHttpClient();
+
+    }
+
+
+
+    /** 웹 서버로 데이터 전송 */
+    private void sendData(String s) {
+        final String data = s;
+        // 네트워크 통신하는 작업은 무조건 작업스레드를 생성해서 호출 해줄 것!!
+        new Thread() {
+            public void run() {
+                /** 웹 서버로 요청을 한다. */
+                RequestBody body = new FormBody.Builder()
+                        .add("parameter", "value")
+                        .add("parameter2", "value")
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://192.168.4.1/"+data)
+                        .post(body)
+                        .build();
+                client.newCall(request).enqueue(callback);
+            }
+        }.start();
+    }
+
+    private final Callback callback = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            Log.d(TAG, "콜백오류:"+e.getMessage());
+        }
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            String body = response.body().string();
+            Log.d(TAG, "서버에서 응답한 Body:"+body);
+        }
+    };
+
+
+    void permission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                ActivityCompat.requestPermissions(
+                        this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(
+                        this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_LOCATION);
+            }
+        }
+    }
+
+    void connect(){
+        WifiConfiguration wificonfig=new WifiConfiguration();
+        wificonfig.SSID = String.format("\"%s\"", "ESP8266-AP");
+        wificonfig.preSharedKey = String.format("\"%s\"", "123456789");
+
+        WifiManager wifiManager=(WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
+        int netId = wifiManager.addNetwork(wificonfig);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
     }
 }
